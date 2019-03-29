a5693bf1be686e77a52830d981214c614240f751
Yousef Aldawoud <yousef.l.t.aldawoud@gmail.com>
2019-03-29 23:12:43 +1300

    Made the converter able to convert multiple files from a directory

--- sign_data_converter.py
+++ sign_data_converter.py
@@ -2,15 +2,18 @@ import sys
 import xlsxwriter
 import os
 
+
 class Converter:
-    def __init__(self, data_file, attributes, result_file, label,
-                 line_splitter = "\n", attributes_splitter = ","):
-        self.label = label
+
+    def __init__(self, data_files, attributes, result_file, person,
+                 line_splitter="\n", attributes_splitter=","):
+        self.line_count = 0
+        self.person = person
         self.attributes = attributes
         self.line_splitter = line_splitter
         self.attributes_splitter = attributes_splitter
-        self.data_file = open(data_file, "r")
-        self.columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
+        self.data_files = data_files
+        self.columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S']
         self.result_file = result_file
         sss = open(result_file, "w")
         sss.close()
@@ -20,45 +23,52 @@ class Converter:
         worksheet = workbook.add_worksheet()
         for count in range(len(self.attributes)):
             worksheet.write(self.columns[count] + '1', self.attributes[count])
-        lines = self.get_file_rows(self.data_file, self.line_splitter)
-        print(len(lines))
-        for i in range(len(lines)):
-            attrs = self.get_row_attributes(lines[i], self.attributes_splitter)
-            for n in range(len(attrs)):
-                worksheet.write(self.columns[n] + str(i + 2), attrs[n])
-            worksheet.write(self.columns[len(attr)-1] + str(i + 2), self.label)
+        file_count = 0
+        for file in self.data_files:
+            file_count += 1
+            lines = self.get_file_rows(file, self.line_splitter)
+            label = file.split("/")[-1].split(".")[0][:-1]
+            for i in range(len(lines) - 1):
+                self.line_count += 1
+                attrs = self.get_row_attributes(lines[i], self.attributes_splitter)
+                for n in range(len(attrs)):
+                    worksheet.write(self.columns[n] + str(self.line_count + 1), attrs[n])
+
+                worksheet.write(self.columns[len(attr) - 2] + str(self.line_count + 1), label)
+                worksheet.write(self.columns[len(attr) - 1] + str(self.line_count + 1), self.person)
+
         workbook.close()
+        print("Converted "+str(len(self.data_files))+" files to "+self.result_file)
 
     @staticmethod
     def get_row_attributes(row, splitter):
         return row.split(splitter)
 
     @staticmethod
-    def get_file_rows(data_file, splitter="\n"):
-        return data_file.read().split(splitter)
+    def get_file_rows(file, splitter="\n"):
+        return open(file, "r").read().split(splitter)
 
     def __str__(self):
         return self.data_file
 
 
 attr = ['x', 'y', 'z', 'roll', 'pitch', 'yaw', 'thumb', 'fore', 'index', 'ring', 'little',
-        'keycode', 'gs1', 'gs2', 'receiver values', 'label']
+        'keycode', 'gs1', 'gs2', 'receiver values', 'label', 'test sample name']
 
 try:
-    label = os.path.basename(sys.argv[1])
-    if len(sys.argv)<3:
-        result_file = sys.argv[1]+".xlsx"
+    print(sys.argv[1].split("/")[1])
+    person = sys.argv[1].split("/")[1]
+    print(os.path.dirname(sys.argv[1]),"sss")
+    if len(sys.argv) < 3:
+        data_file = [f for f in os.listdir(sys.argv[1]) if os.path.isfile(os.path.join(sys.argv[1], f))]
+        result_file = person + ".xlsx"
+        label = data_file[0]
+        print(data_file[0])
     else:
         result_file = sys.argv[2]
-    c = Converter(sys.argv[1], attr, result_file,label)
+    data_file = [os.path.join(sys.argv[1], f) for f in os.listdir(sys.argv[1]) if
+                 os.path.isfile(os.path.join(sys.argv[1], f))]
+    c = Converter(data_file, attr, result_file, person=person)
     c.convert()
 except IndexError:
-
-    try:
-        data_file = input("What's the data file directory ?")
-        result_file = input("What's the result file directory ?")
-        label = input("what is the label ?")
-        c = Converter(data_file, attr, result_file,label)
-        c.convert()
-    except FileNotFoundError:
-        print('file not found')
+    print('file not found')
